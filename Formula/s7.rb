3aa8c7c9e5a1068b300ee25c9640a54396529c1b
Pavlo Shkrabliuk <pastey@readdle.com>
2021-02-04 15:39:07 +0200

    publish new s7 version (1.0) to Homebrew

--- Formula/s7.rb
+++ Formula/s7.rb
@@ -4,9 +4,9 @@ class S7 < Formula
   homepage "https://github.com/readdle/system7"
   license "MIT"
 
-  url "https://github.com/readdle/system7/releases/download/v0.9/s7"
-  sha256 "6468959b19c94b2eb9c0ed764aa5dca78e4957c5effed16c6018ddc55802892a"
-  version "0.9" # have to specify version explicitly as automatic detection is confused by 7 in the name of s7 :)
+  url "https://github.com/readdle/system7/releases/download/v1.0/s7"
+  sha256 "fcbcc4d2cefa5ed9e3bf8e2bcd13ab016ced2ec7bb52f245d8c4fbc5ccc9adcb"
+  version "1.0" # have to specify version explicitly as automatic detection is confused by 7 in the name of s7 :)
 
   def install
     bin.install "s7"
