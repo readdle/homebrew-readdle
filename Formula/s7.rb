30f129c732fd5a8a08656eb16545a6627781cd8d
Pavlo Shkrabliuk <pastey@readdle.com>
2024-07-27 22:51:40 +0300

    Update s7.rb
    Rollback formula to a working 1.0 release.

--- Formula/s7.rb
+++ Formula/s7.rb
@@ -4,8 +4,8 @@ class S7 < Formula
   homepage "https://github.com/readdle/system7"
   license "MIT"
 
-  url "https://github.com/readdle/system7/releases/download/v1.0.1/s7.tar.gz"
-  sha256 "9dd6129c62120d2f26d622e752d62a821853a6b9c6125fe2795e9cc7139e0d71"
+  url "https://github.com/readdle/system7/releases/download/v1.0/s7"
+  sha256 "fcbcc4d2cefa5ed9e3bf8e2bcd13ab016ced2ec7bb52f245d8c4fbc5ccc9adcb"
   version "1.0" # have to specify version explicitly as automatic detection is confused by 7 in the name of s7 :)
 
   def install
